e1fc3ebfbec85f21ded871ed80dc0413fcec0c96
FarabyMalau <farabymuzakki49@gmail.com>
2025-05-15 19:28:16 +0600

    Step 4 if current is not the last node

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -114,7 +114,12 @@ public:
             // Step 3: Link previous node to the next of current
             current->prev->next = current->next;
 
+            // Step 4: if current is not the last node
+            if (current->next != NULL)
+                 current->next->prev = current->prev;
         }
+
+        // 
 
     } 
         
